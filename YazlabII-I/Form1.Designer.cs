b986c7fb9de476d3f7742873c7516114280ea87a
Ramazan Demir <rdemir057@gmail.com>
2017-03-06 20:25:13 +0300

    project upload

---
+++ YazlabII-I/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace YazlabII_I
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.beginDot = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EndDot = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.iterasyon = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlangıç Noktası:";
+            // 
+            // beginDot
+            // 
+            this.beginDot.Location = new System.Drawing.Point(115, 17);
+            this.beginDot.Name = "beginDot";
+            this.beginDot.Size = new System.Drawing.Size(118, 20);
+            this.beginDot.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bitiş Noktası:";
+            // 
+            // EndDot
+            // 
+            this.EndDot.Location = new System.Drawing.Point(322, 17);
+            this.EndDot.Name = "EndDot";
+            this.EndDot.Size = new System.Drawing.Size(118, 20);
+            this.EndDot.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "İterasyon Sayısı:";
+            // 
+            // iterasyon
+            // 
+            this.iterasyon.Location = new System.Drawing.Point(543, 17);
+            this.iterasyon.Name = "iterasyon";
+            this.iterasyon.Size = new System.Drawing.Size(118, 20);
+            this.iterasyon.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(17, 78);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(216, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(322, 75);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Başlat";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 321);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.iterasyon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EndDot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.beginDot);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox beginDot;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EndDot;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox iterasyon;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ YazlabII-I/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazlabII_I
+{
+    public partial class Form1 : Form
+    {
+        public static string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            textBox4.Text = file.FileName;
+            path = file.FileName;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+            String line;
+            double[][] Rmatris = new double[9][];
+            int i = 0,j=0;
+            for (i = 0; i < 9; i++)
+            {
+                Rmatris[i] = new double[9];
+                for (j = 0; j < 9; j++)
+                {
+                    Rmatris[i][j] = -1;
+                }
+            }
+            i = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ( (line = sr.ReadLine() ) != null)
+                {                   
+                    var satir = line.Split(',');
+                    foreach (var item in satir)
+                    {
+                        if (i == Convert.ToDouble(EndDot.Text))
+                        {
+                            foreach (var mundi in satir)
+                            {
+                                Rmatris[Convert.ToInt32(mundi)][i] = 100;
+                            }
+                        }
+                        if (Rmatris[i][Convert.ToInt32(item)] != 100)
+                        {
+                            Rmatris[i][Convert.ToInt32(item)] = 0;
+                        }
+                        
+                    }
+                    i++;
+                }
+            }
+            using (StreamWriter wr = new StreamWriter("C:\\Users\\Ramazan Demir\\Desktop\\Rmatrisi.txt",false))
+            {
+                for (i = 0; i < 9; i++)
+                {
+                    for (j = 0; j < 9; j++)
+                    {
+                        wr.Write(Rmatris[i][j] +  " ");
+                    }
+                    wr.WriteLine();
+                }
+            }
+            MessageBox.Show("Tamamlandı ve Bitti");
+            var Qmatris = Qmatris_Olustur();
+
+            for (int z = 0; z < Convert.ToInt32(iterasyon.Text); z++)
+            {
+                int baslangicDot = Convert.ToInt32(beginDot.Text);
+
+                //baslangic random olarak seçilir vve olaylar gelişir
+                string yol = "";
+                while (true)
+                {
+
+
+                    for (i = 0; i < 9; i++)
+                    {
+                        if (Rmatris[baslangicDot][i] >= 0)
+                        {
+                            yol = yol + i + "|";
+                        }
+                    }
+
+                    var yollar = yol.Split('|');
+
+                    if (baslangicDot == 4)
+                    {
+                        var dummy = 1;
+                    }
+                    Random lRandom = new Random();
+                    int randomIndex = lRandom.Next(0, yollar.Length - 1);
+                    int qbaslangic = Convert.ToInt32(yollar[randomIndex]);
+
+                    /*if (baslangicDot == Convert.ToInt32(EndDot.Text))
+                    {
+                        break;
+                    }*/
+
+                    var sonuc = zincir(Rmatris, baslangicDot, qbaslangic, Qmatris);
+                    yol = "";
+                    
+                    Qmatris[baslangicDot][qbaslangic] = Qmatris[baslangicDot][qbaslangic] +  sonuc[1];
+                    baslangicDot = Convert.ToInt32(sonuc[0]);
+                }
+
+            }
+
+            using (StreamWriter wr = new StreamWriter("C:\\Users\\Ramazan Demir\\Desktop\\Qmatris.txt", false))
+            {
+                for (i = 0; i < 9; i++)
+                {
+                    for (j = 0; j < 9; j++)
+                    {
+                        wr.Write(Qmatris[i][j] + " ");
+                    }
+                    wr.WriteLine();
+                }
+            }
+            
+
+
+        }
+
+        protected double[] zincir(double[][] Rmatris, int indexQbegin,int indexQend,double[][] Qmatris)
+        {
+            int i;
+            string yol = "";
+            for (i = 0; i < 9; i++)
+            {
+                if (Rmatris[indexQend][i] >= 0)
+                {
+                    yol = yol + i + "|";
+                }
+            }
+            var yollar = yol.Split('|');
+            yollar = yollar.Take(yollar.Count() - 1).ToArray();
+
+            int[] myInts = Array.ConvertAll(yollar, s => int.Parse(s));
+
+            yol = "";
+
+            for (i = 0; i < myInts.Length; i++)
+            {
+                yol = yol + Qmatris[indexQend][myInts[i]] + "|";  
+            }
+
+            var SonucYollar = yol.Split('|');
+            SonucYollar = SonucYollar.Take(SonucYollar.Count() - 1).ToArray();
+
+            int[] MaxQ = Array.ConvertAll(SonucYollar, s => int.Parse(s));
+
+            int maxValue = MaxQ.Max();
+
+            double[] ReturnSonuc = new double[2];
+
+            string randomSec = "";
+
+            for (i = 0; i < myInts.Length; i++)
+            {
+                if (Qmatris[indexQend][myInts[i]] == maxValue)
+                {
+                    //aynı max degerler gelirse
+                    ReturnSonuc[0] = myInts[i];
+                    randomSec = randomSec + myInts[i] + "|";
+                    
+                }
+            }
+            // maxlar aynı olursa random seç
+            var SonucRandomSec = randomSec.Split('|');
+
+            SonucRandomSec = SonucRandomSec.Take(SonucRandomSec.Count() - 1).ToArray();
+            
+            Random lRandom = new Random();
+            int randomIndex = lRandom.Next(0, SonucRandomSec.Length);
+
+            ReturnSonuc[0] = Convert.ToDouble(SonucRandomSec[randomIndex]);
+            // random seç bitti
+            
+            
+            double sonuc = (maxValue * 0.8) + Rmatris[indexQbegin][indexQend];
+
+
+            ReturnSonuc[1] = sonuc;
+
+            return ReturnSonuc;
+        }
+        protected double[][] Qmatris_Olustur()
+        {
+            double[][] Qmatris = new double[9][];
+            int i, j;
+            for (i = 0; i < 9; i++)
+            {
+                Qmatris[i] = new double[9];
+                for (j = 0; j < 9; j++)
+                {
+                    Qmatris[i][j] = 0;
+                }
+            }
+            return Qmatris;
+        }
+
+    }
+}
